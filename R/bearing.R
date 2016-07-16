554521e08f280efc6466cfdeca27a79024a28a3d
Soonmok Kwon <ep1804@gmail.com>
2016-07-17 02:13:24 +0900

    bearing data changed

--- R/bearing.R
+++ R/bearing.R
@@ -1,20 +1,16 @@
-#' Publicly available bearing run-to-fail data. Reduced version.
+#' Publicly available bearing run-to-fail data, 
 #' 
-#' Experiments on bearings. The data set was provided by the Center for 
-#' Intelligent Maintenance Systems, University of Cincinnati.
-#' First set is reduced from 20 kHz to 1 Hz frequency.
+#' Originally provided by the Center for Intelligent Maintenance Systems, 
+#' University of Cincinnati.
+#' Among the original data set, second set is selected and filtered.
 #' 
-#' @format A data frame with 2156 rows and 9 variables:
+#' @format A data frame with 984 rows and 5 variables:
 #' \describe{
 #'   \item{Time}{time of measurement}
-#'   \item{V1}{Accelerometer}
-#'   \item{V2}{Accelerometer}
-#'   \item{V3}{Accelerometer}
-#'   \item{V4}{Accelerometer}
-#'   \item{V5}{Accelerometer}
-#'   \item{V6}{Accelerometer}
-#'   \item{V7}{Accelerometer}
-#'   \item{V8}{Accelerometer}
+#'   \item{Ch1}{Accelerometer}
+#'   \item{Ch2}{Accelerometer}
+#'   \item{Ch3}{Accelerometer}
+#'   \item{Ch4}{Accelerometer}
 #' }
 #' @source \url{http://ti.arc.nasa.gov/tech/dash/pcoe/prognostic-data-repository/}
 "bearing"
