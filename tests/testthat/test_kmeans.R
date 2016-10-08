95c487936e196d5f846afb7c644a94fc3e657162
Soonmok Kwon <ep1804@gmail.com>
2016-10-09 02:17:38 +0900

    kmeansruns wip

--- tests/testthat/test_kmeans.R
+++ tests/testthat/test_kmeans.R
@@ -1,28 +1,10 @@
 context("K-Means clustering")
 
-test_that("Check kmeans performance result of iris[,1]", {
-  set.seed(1)
-  
-  kf <- el.kmeansPerf(iris[,1:4])
-  
-  expect_true(is.numeric(kf))
-  expect_equal(length(kf), 15)
-})
-
-test_that("Check kmeans performance result of iris[,1:4]", {
-  set.seed(1)
-  
-  kf <- el.kmeansPerf(iris[,1:4])
-  
-  expect_true(is.numeric(kf))
-  expect_equal(length(kf), 15)
-})
-
 test_that("Check kmeans result of iris[,1:4]", {
   set.seed(1)
   
-  d <- iris[,1:4]
-  cl <- el.kmeans(d, 3)
+  d <- iris[,-5]
+  cl <- el.kmeans(d)
   score <- el.kmeansScore(d, cl$fit)
   
   expect_equal(cl$score, score)
