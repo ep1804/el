f7e6cb94ea3dcd282b8ef66fae46ecae6198d8fb
soonmok-kwon <soonmok.kwon@navercorp.com>
2017-07-12 00:04:00 +0900

    re.model

---
+++ R/re.model.R
@@ -0,0 +1,197 @@
+requireNamespace('caret')
+requireNamespace('caretEnsemble')
+requireNamespace('glmnet')
+requireNamespace('ranger')
+requireNamespace('xgboost')
+requireNamespace('kernlab')
+requireNamespace('deepnet')
+requireNamespace('lattice')
+requireNamespace('stats')
+requireNamespace('ggplot2')
+requireNamespace('reshape2')
+
+
+#' Fit a regression model with CV-based parameter tuning, and show in-sample performance
+#'
+#' @param x       numeric or factor data.frame.
+#' @param y       numeric or factor vector.
+#' @param cvFolds Cross-validation fold number
+#' @param size.lim numeric. If data size is larger than this, it is sampled.
+#' @param plot    logical. Plot or not
+#'
+#' @return list of final models
+#' @export
+#'
+#' @examples
+#' dia <- ggplot2::diamonds
+#' dia <- dia[sample(1:nrow(dia), 800),]
+#' x <- dia
+#' x$price <- NULL
+#' y <- dia$price
+#'
+#' fits <- el.re.model(x, y)
+#'
+el.re.model <- function(x, y, cvFolds = 7, size.lim = 10000, plot = TRUE) {
+
+  if (!is.vector(y) | !is.numeric(y)) {
+    logger.error("y should be a numeric vector.")
+    return()
+  }
+
+  y <- as.vector(y) # for the some vector subclasses that violates LSP
+
+  if (!is.data.frame(x)) {
+    logger.error("x should be a data.frame.")
+    return()
+  }
+
+  x <- as.data.frame(x) # for the some data.frame subclasses that violates LSP
+  colnames(x) <- sapply(colnames(x), function(s){ gsub(" ", "_", s) })
+
+  if (nrow(x) > size.lim) {
+    wh <- sort(sample(nrow(x), size.lim))
+    y <- y[wh]
+    x <- x[wh,]
+  }
+
+  folds <- caret::createFolds(y, k = cvFolds)
+
+  trControl <- caret::trainControl(
+    method = 'cv',
+    number = length(folds),
+    verboseIter = TRUE,
+    savePredictions = TRUE,
+    index = folds
+  )
+
+  # For linear regression, expanding factors to set of numeric varibles
+  xn <- if (all(sapply(x, is.numeric))) {
+    x
+  } else {
+    logger.info("For some models, factor variables are expanded to numerical variables.")
+
+    res <- data.frame(matrix(nrow = nrow(x)))
+
+    for (cl in colnames(x)) {
+      if (is.logical(x[, cl])) {
+        add <- as.matrix(as.numeric(x[, cl]))
+        colnames(add) <- cl
+        res <- cbind(res, add)
+      } else if (is.factor(x[, cl])) {
+        fm <- paste('~', cl, '- 1')
+        add <- stats::model.matrix(stats::as.formula(fm), data = x)
+        randomize <- diff(range(add)) / 1000000 * rnorm(nrow(add) * ncol(add))
+        randomize <- matrix(randomize, ncol = ncol(add))
+        add <- add + randomize
+
+        res <- cbind(res, add)
+      } else {
+        add <- as.matrix(x[, cl])
+        colnames(add) <- cl
+        res <- cbind(res, add)
+      }
+    }
+
+    colnames(res) <- sapply(colnames(res), function(s){ gsub(" ", "_", s) })
+    res[, -1]
+  }
+
+  # Fit regularized linear regression model with CV-based parameter tuning
+  fit.lm <- caret::train(
+    x = xn,
+    y = y,
+    method = 'glmnet',
+    trControl = trControl
+  )
+
+  # Fit random forest model with CV-based parameter tuning
+  fit.rf <- caret::train(
+    x = x,
+    y = y,
+    method = 'ranger',
+    importance = 'impurity',
+    trControl = trControl
+  )
+
+  # Fit gradient boosted reg. tree model with CV-based parameter tuning
+  fit.gbrt <- caret::train(
+    x = xn,
+    y = y,
+    method = 'xgbTree',
+    trControl = trControl
+  )
+
+  # Fit SVM model with CV-based parameter tuning
+  fit.svm <- caret::train(
+    x = xn,
+    y = y,
+    method = 'svmRadial',
+    preProcess = c("center", "scale"),
+    trControl = trControl
+  )
+
+  # Fit neural network model with CV-based parameter tuning
+  fit.nn <- caret::train(
+    x = xn,
+    y = y,
+    method = 'brnn',
+    preProcess = c("center", "scale"),
+    trControl = trControl
+  )
+
+  if (plot) {
+
+    plotPred <- function(y, pred, main) {
+      df <- data.frame(y, prediction = pred)
+      df <- data.frame(index = 1:length(y), df[order(y),])
+      df <- reshape2::melt(df, id.vars = 'index')
+      g <-
+        ggplot2::ggplot(data = df, aes(x = index, y = value, col = variable)) +
+        ggplot2::geom_jitter(alpha = 0.5) +
+        ggplot2::ggtitle(main)
+      print(g)
+    }
+
+    # Tuning on training parameters and predictions on training data
+    print(plot(fit.lm, main = 'Linear model tuning'))
+    plotPred(y, predict(fit.lm), 'LM prediction on train data')
+    print(plot(caret::varImp(fit.lm), main = 'Variable importance by LM'))
+
+    print(plot(fit.rf, main = 'RF tuning'))
+    plotPred(y, predict(fit.rf), 'RF model prediction on train data')
+    print(plot(caret::varImp(fit.rf), main = 'Variable importance by RF'))
+
+    print(plot(fit.gbrt, main = 'GBRT tuning'))
+    plotPred(y, predict(fit.gbrt), 'GBRT model prediction on train data')
+    print(plot(caret::varImp(fit.gbrt), main = 'Variable importance by GBRT'))
+
+    print(plot(fit.svm, main = 'SVM tuning'))
+    plotPred(y, predict(fit.svm), 'SVM model prediction on train data')
+    print(plot(caret::varImp(fit.svm), main = 'Variable importance by SVM'))
+
+    print(plot(fit.nn, main = 'NN tuning'))
+    plotPred(y, predict(fit.nn), 'NN model prediction on train data')
+    print(plot(caret::varImp(fit.nn), main = 'Variable importance by NN'))
+  }
+
+  # Compare models
+  fits <-
+    list(
+      LM = fit.lm,
+      RF = fit.rf,
+      GBRT = fit.gbrt,
+      SVM = fit.svm,
+      NN = fit.nn
+    )
+  fits.comp <- caret::resamples(fits)
+  logger.info("Model comparison summary:", summary(fits.comp), capture = T)
+
+  if (plot) {
+    print(lattice::bwplot(fits.comp, metric = 'RMSE', main = 'Model comparison by RMSE'))
+    print(
+      lattice::bwplot(fits.comp, metric = 'Rsquared', main = 'Model comparison by R-squared')
+    )
+  }
+
+  fits
+}
---
+++ tests/testthat/test_re_model.R
@@ -0,0 +1,40 @@
+context("caret Regression Modeling")
+
+test_that("Check regression modeling", {
+
+  # http://r-pkgs.had.co.nz/check.html says:
+  # Occasionally you may have a problem where the tests pass when run
+  # interactively with devtools::test(), but fail when in R CMD check.
+  # This usually indicates that you’ve made a faulty assumption about
+  # the testing environment, and it’s often hard to figure it out.
+  #
+  # I had this situation: For some reason I don't know, set.seed()
+  # didn't work and the final test failed. Than, I occationally
+  # found the set.seed() didn't work just after restarting R session.
+  # After some dansing, I've found a walkaround of inserting
+  # following two requireNamespace() lines.
+  #
+  requireNamespace('caret')
+  requireNamespace('caretEnsemble')
+
+  set.seed(1234)
+
+  x <- iris[,1:4]
+  y <- as.numeric(iris$Species)
+  fits <- el.re.model(x, y)
+
+  expect_true(is.list(fits))
+  expect_equal(round(fits$LM$pred$pred[1], 5), 1.06673)
+
+  set.seed(1234)
+
+  dia <- ggplot2::diamonds
+  dia <- dia[sample(1:nrow(dia), 800),]
+  x <- dia
+  x$price <- NULL
+  y <- dia$price
+  fits <- el.re.model(x, y)
+
+  expect_true(is.list(fits))
+  expect_equal(round(fits$LM$pred$pred[1], 5), 4629.36736)
+})
